b162c28f25b55819af5407392f502e7dbb74e999
Erik Mauk <e.mauk1999@gmail.com>
2019-04-08 10:52:43 -0700

    fixed indentation errors

--- lab1.py
+++ lab1.py
@@ -4,14 +4,14 @@ def max_list_iter(int_list):  # must use iteration not recursion
    If int_list is empty, returns None. If list is None, raises ValueError"""
    
    if int_list == None:
-   	raise ValueError
+      raise ValueError
    elif len(int_list) == 0:
-   	return None
+      return None
    else:
-   	maxNum = int_list[0]
-   	for i in range(len(int_list)):
-   		if int_list[i] > maxNum:
-   			maxNum = int_list[i]
+      maxNum = int_list[0]
+      for i in range(len(int_list)):
+         if int_list[i] > maxNum:
+            maxNum = int_list[i]
 
    return maxNum
 
@@ -19,29 +19,29 @@ def reverse_rec(int_list):   # must use recursion
    """recursively reverses a list of numbers and returns the reversed list
    If list is None, raises ValueError"""
    if int_list == None:
-   	raise ValueError
+      raise ValueError
    if len(int_list) == 0:
-   	return int_list
+      return int_list
    else:
-   	return [int_list[-1]] + reverse_rec(int_list[:-1])
+      return [int_list[-1]] + reverse_rec(int_list[:-1])
 
 def bin_search(target, low, high, int_list):  # must use recursion
    """searches for target in int_list[low..high] and returns index if found
    If target is not found returns None. If list is None, raises ValueError """
    if int_list == None:
-   	raise ValueError
+      raise ValueError
 
    if low > high:
-   	return None
+      return None
 
    midpoint = (low + high) // 2
 
    if int_list[midpoint] == target:
-   	return midpoint
+      return midpoint
 
    if int_list[midpoint] < target:
-   	return bin_search(target, midpoint+1, high, int_list)
-   	
-  	if int_list[midpoint] > target:
-   	return bin_search(target, low, midpoint-1, int_list)
+      return bin_search(target, midpoint+1, high, int_list)
+
+   if int_list[midpoint] > target:
+      return bin_search(target, low, midpoint-1, int_list)
 
